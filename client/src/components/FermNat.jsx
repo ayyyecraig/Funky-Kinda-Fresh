f0ddcc48e31a0b4080616c33cec7fa2cb096f407
Adelaide Craig <adelaide.craig@outlook.com>
2022-03-30 22:36:39 -0500

    styling 10:30

--- client/src/components/FermNat.jsx
+++ client/src/components/FermNat.jsx
@@ -34,10 +34,12 @@ import { Link } from 'react-router-dom'
               <div className="Grid">
               <li className="ferments" key={idx}>
       
-                <img src={ferment.img} className="img" />
+                <img src={ferment.img} className="img2" />
+                <div className="text">
                 <h4>{ferment.name}</h4>
                 <h5>{ferment.typeFerment}</h5>
-                <p>{ferment.description}</p>
+                <p className="ptext">{ferment.description}</p>
+                </div>
               </li>
               <Link to={`/FermNat/${ferment._id}/update`}><button className="manage"  >Manage</button></Link>
               <button className="delete" onClick={()=> deleteFerm(ferment._id)}>Delete</button>
